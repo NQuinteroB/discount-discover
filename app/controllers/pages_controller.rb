bc55b497a30f06e57479511881deed9e1abe4399
Diego Rodriguez Perez <diegorper@gmail.com>
2022-09-01 13:15:40 +0200

    Done with style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,6 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [ :dashboard ]
   skip_before_action :authenticate_user!, only: [ :index ]
 
-  def home
-  end
-
   def index
     @stores = Store.all
   end
