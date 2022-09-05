470166a403a0aaafc34b3e12cddf8b1c6e43edae
johnson-darko <johnsondarko365@gmail.com>
2022-09-05 15:18:09 +0200

    done with review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :store
+  validates :rating, numericality: { only_integer: true }
+ validates :rating, inclusion: { in: 0..5 }
 end
