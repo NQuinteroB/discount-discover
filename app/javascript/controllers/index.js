e6555075cd882599bdae1955c8f28ee97bd347f1
Diego Rodriguez Perez <diegorper@gmail.com>
2022-09-08 12:30:51 +0200

    Done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import SearchbarController from "./searchbar_controller"
+application.register("searchbar", SearchbarController)
